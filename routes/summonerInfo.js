68830e840c2a8eaa5f4130c08bbee25fb449c3dc
kingbj940429 <kingbj0429@gmail.com>
2020-09-22 22:53:52 +0900

    Add 5 Games Information

--- routes/summonerInfo.js
+++ routes/summonerInfo.js
@@ -8,24 +8,31 @@ var matchDto = require('../axios/matchDto');
 var test = require('../routerJS/test.js');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    
+    var participantList =[];
     const getSummoner_information = async () => {
         try {
             const searchedName = req.query.summonerName;//소환사 이름 ex) 여의도 한량이
 
             const searchedSummonerId = await getSummonerId(searchedName);//쿼리스트링의 이름을 axios.getSummonerId에 넣어서 값 받기 AccountId : searchedSummonerId.data.accountId
-        
-            const summoner_getGameId = await getGameId(searchedSummonerId.data.accountId); //gameId : summoner_getGameId.data.matches[0].gameId
-           
-            test(searchedName);
-           
-            const MatchDto = await matchDto(summoner_getGameId.data.matches[0].gameId);
-              
-            console.log(MatchDto.data.teams[0].win);
-            
-            //summonerName : MatchDto.data.participantIdentities[0].player.summonerName
             
             const summoner = await getSummonerInfo(searchedSummonerId.data.id);//소환사 기본 정보 콜백함수
+
+            const summoner_getGameId = await getGameId(searchedSummonerId.data.accountId); //gameId : summoner_getGameId.data.matches[0].gameId
+            
+            //test(searchedName);
+
+            const MatchDto = [];
+            for(i=0;i<5;i++){//총 5개의 나타내기 위함
+                 MatchDto[i] = await matchDto(summoner_getGameId.data.matches[i].gameId);
+            }
+            for(k=0;k<5;k++){
+                participantList[k] = [];
+                for(i=0; i<MatchDto[k].data.participantIdentities.length;i++){
+                    participantList[k].push(MatchDto[k].data.participantIdentities[i].player.summonerName);
+                }
+            }
+            console.log(participantList);
+            
                 res.render('summonerInfo.pug', 
                 {
                     summonername: summoner.data[0].summonerName,
@@ -34,17 +41,7 @@ router.get('/', function (req, res, next) {
                     leaguePoints: summoner.data[0].leaguePoints,
                     wins: summoner.data[0].wins,
                     losses: summoner.data[0].losses,
-                    participantIdentities01 : MatchDto.data.participantIdentities[0].player.summonerName,//생으로 코딩하기 싫은데..
-                    participantIdentities02 : MatchDto.data.participantIdentities[1].player.summonerName,
-                    participantIdentities03 : MatchDto.data.participantIdentities[2].player.summonerName,
-                    participantIdentities04 : MatchDto.data.participantIdentities[3].player.summonerName,
-                    participantIdentities05 : MatchDto.data.participantIdentities[4].player.summonerName,
-                    participantIdentities06 : MatchDto.data.participantIdentities[5].player.summonerName,
-                    participantIdentities07 : MatchDto.data.participantIdentities[6].player.summonerName,
-                    participantIdentities08 : MatchDto.data.participantIdentities[7].player.summonerName,
-                    participantIdentities09 : MatchDto.data.participantIdentities[8].player.summonerName,
-                    participantIdentities10 : MatchDto.data.participantIdentities[9].player.summonerName
-
+                    result: participantList,
                 });
             
         } catch (error) {
