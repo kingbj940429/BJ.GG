5d095d5cfc06260578cbe25ffbac361d7e203360
kingbj940429 <kingbj0429@gmail.com>
2020-09-25 17:44:16 +0900

    승패에 따른 색깔변화

--- routerJS/matchDto.js
+++ routerJS/matchDto.js
@@ -1,46 +1,46 @@
 var matchDto = require('../axios/matchDto');
 
-const participantIdentities = async(summoner_getGameId, searchedName) => {
+const participantIdentities = async (summoner_getGameId, searchedName) => {
     try {
-        var participantList ={};
-        const searchedName_eachGame_number = [];
+        var participantList = {}; //게임정보 다 들어가 있음
+        const searchedName_eachGame_number = []; //검색된 소환사의 각 게임당 위치를 저장
         const MatchDto = [];
-        var team_number = [];
-        for (i = 0; i < 5; i++) { //최근 5개의 게임만을 나타내기 위함
+        var team_number = []; //검색된 소환사가 어느 팀이였는지 구별해주는 변수
+        var game_of_times = 5; //게임 횟수
+
+        for (i = 0; i < game_of_times; i++) { //최근 5개의 게임만을 나타내기 위함
             MatchDto[i] = await matchDto(summoner_getGameId.data.matches[i].gameId);
         }
-        for (k = 0; k < 5; k++) {
+        for (k = 0; k < game_of_times; k++) {
             participantList[k] = [];
             for (i = 0; i < MatchDto[k].data.participantIdentities.length; i++) {
                 participantList[k].push(MatchDto[k].data.participantIdentities[i].player.summonerName);
-                if(MatchDto[k].data.participantIdentities[i].player.summonerName === searchedName){
-                    searchedName_eachGame_number[k] = i;//검색된 소환사의 이름의 순번. index이므로 0부터 시작
+                if (MatchDto[k].data.participantIdentities[i].player.summonerName === searchedName) {
+                    searchedName_eachGame_number[k] = i; //검색된 소환사의 이름의 순번. index이므로 0부터 시작
                 }
             }
-            if( searchedName_eachGame_number[k] < 5){
+            if (searchedName_eachGame_number[k] < game_of_times) {
                 team_number[k] = 0;
-            }else{
+            } else {
                 team_number[k] = 1;
             }
         }
-        
-        var k=5;
+
+
         var team_number_count = 0;
-        for(i=0;i<searchedName_eachGame_number.length;i++){
-            participantList[k] = {
-                gameTime : Math.round(MatchDto[i].data.gameDuration/60),
-                gameWinFail : MatchDto[i].data.teams[team_number[team_number_count]].win,//이거 어느 팀이냐에 따라 다르게 나와야함
-                championId : MatchDto[i].data.participants[searchedName_eachGame_number[i]].championId,
-                kills : MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.kills,
-                deaths : MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.deaths,
-                assists : MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.assists,
+        for (i = 0; i < searchedName_eachGame_number.length; i++) {
+            participantList[game_of_times] = {
+                gameTime: Math.round(MatchDto[i].data.gameDuration / 60),
+                gameWinFail: MatchDto[i].data.teams[team_number[team_number_count]].win, //이거 어느 팀이냐에 따라 다르게 나와야함
+                championId: MatchDto[i].data.participants[searchedName_eachGame_number[i]].championId,
+                kills: MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.kills,
+                deaths: MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.deaths,
+                assists: MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.assists,
             }
-            //console.log(MatchDto[i].data.participantIdentities[searchedName_eachGame_number[i]].player.summonerName);
-            //console.log(MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.kills);//5게임의 킬수 잘들어감.
-            k++;
+            game_of_times++;
             team_number_count++;
         }
-        
+
         //console.log(participantList);
         return participantList;
     } catch (error) {
--- routes/summonerInfo.js
+++ routes/summonerInfo.js
@@ -8,42 +8,31 @@ var matchDto = require('../axios/matchDto');
 var matchDto_JS = require('../routerJS/matchDto.js');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    
+
     const getSummoner_information = async () => {
         try {
-            const searchedName = req.query.summonerName;//소환사 이름 ex) 여의도 한량이
+            const searchedName = req.query.summonerName; //소환사 이름 ex) 여의도 한량이
 
-            const searchedSummonerId = await getSummonerId(searchedName);//쿼리스트링의 이름을 axios.getSummonerId에 넣어서 값 받기 AccountId : searchedSummonerId.data.accountId
-            
-            const summoner = await getSummonerInfo(searchedSummonerId.data.id);//소환사 기본 정보 콜백함수
+            const searchedSummonerId = await getSummonerId(searchedName); //쿼리스트링의 이름을 axios.getSummonerId에 넣어서 값 받기 AccountId : searchedSummonerId.data.accountId
+
+            const summoner = await getSummonerInfo(searchedSummonerId.data.id); //소환사 기본 정보 콜백함수
 
             const summoner_getGameId = await getGameId(searchedSummonerId.data.accountId); //gameId : summoner_getGameId.data.matches[0].gameId
-            
-            const participantList = await matchDto_JS(summoner_getGameId, searchedName);//소환사 10명 모두 가져옴
+
+            const participantList = await matchDto_JS(summoner_getGameId, searchedName); //소환사 10명 모두 가져옴
 
             const MatchDto = await matchDto(summoner_getGameId.data.matches[0].gameId);
-            // const participant_game_info = {
-            //     gameTime : Math.round(MatchDto.data.gameDuration/60),
-            //     gameWinFail : MatchDto.data.teams[0].win,
-            //     championId : MatchDto.data.participants[0].championId,
-            //     kills : MatchDto.data.participants[0].stats.kills,
-            //     deaths : MatchDto.data.participants[0].stats.deaths,
-            //     assists : MatchDto.data.participants[0].stats.assists,
-            // }
-            // console.log(participant_game_info);
-          
-            
-            res.render('summonerInfo.pug', 
-                {
-                    summonername: summoner.data[0].summonerName,
-                    queueType: summoner.data[0].queueType,
-                    tier: summoner.data[0].tier,
-                    leaguePoints: summoner.data[0].leaguePoints,
-                    wins: summoner.data[0].wins,
-                    losses: summoner.data[0].losses,
-                    result: participantList,
-                });
-            
+
+            res.render('summonerInfo.pug', {
+                summonername: summoner.data[0].summonerName,
+                queueType: summoner.data[0].queueType,
+                tier: summoner.data[0].tier,
+                leaguePoints: summoner.data[0].leaguePoints,
+                wins: summoner.data[0].wins,
+                losses: summoner.data[0].losses,
+                result: participantList,
+            });
+
         } catch (error) {
             console.log(error);
         }
