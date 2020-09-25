53a1d7f06708d276464412d44ceb8e09d338e1ce
kingbj940429 <kingbj0429@gmail.com>
2020-09-25 12:29:17 +0900

    승패 기능 추가

--- routerJS/matchDto.js
+++ routerJS/matchDto.js
@@ -5,6 +5,7 @@ const participantIdentities = async(summoner_getGameId, searchedName) => {
         var participantList ={};
         const searchedName_eachGame_number = [];
         const MatchDto = [];
+        var team_number = [];
         for (i = 0; i < 5; i++) { //최근 5개의 게임만을 나타내기 위함
             MatchDto[i] = await matchDto(summoner_getGameId.data.matches[i].gameId);
         }
@@ -16,12 +17,19 @@ const participantIdentities = async(summoner_getGameId, searchedName) => {
                     searchedName_eachGame_number[k] = i;//검색된 소환사의 이름의 순번. index이므로 0부터 시작
                 }
             }
+            if( searchedName_eachGame_number[k] < 5){
+                team_number[k] = 0;
+            }else{
+                team_number[k] = 1;
+            }
         }
+        
         var k=5;
+        var team_number_count = 0;
         for(i=0;i<searchedName_eachGame_number.length;i++){
             participantList[k] = {
                 gameTime : Math.round(MatchDto[i].data.gameDuration/60),
-                gameWinFail : MatchDto[i].data.teams[0].win,//이거 어느 팀이냐에 따라 다르게 나와야함
+                gameWinFail : MatchDto[i].data.teams[team_number[team_number_count]].win,//이거 어느 팀이냐에 따라 다르게 나와야함
                 championId : MatchDto[i].data.participants[searchedName_eachGame_number[i]].championId,
                 kills : MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.kills,
                 deaths : MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.deaths,
@@ -30,9 +38,10 @@ const participantIdentities = async(summoner_getGameId, searchedName) => {
             //console.log(MatchDto[i].data.participantIdentities[searchedName_eachGame_number[i]].player.summonerName);
             //console.log(MatchDto[i].data.participants[searchedName_eachGame_number[i]].stats.kills);//5게임의 킬수 잘들어감.
             k++;
+            team_number_count++;
         }
         
-        console.log(participantList);
+        //console.log(participantList);
         return participantList;
     } catch (error) {
         console.error(error);
