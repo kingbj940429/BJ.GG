a1d2bef43c1b6bfb646e8f7348dc5af600598cbd
kingbj940429 <kingbj0429@gmail.com>
2020-10-11 19:58:31 +0900

    각각 3게임씩 가져옴

--- axios/getGameId.js
+++ axios/getGameId.js
@@ -1,12 +1,12 @@
 //소환사의 이름을 검색했을 때 summonerId를 가져와 줍니다.
 const axios = require('axios');
 
-const getGameId = async (accountId) => {
+const getGameId = async (accountId, add_game_count) => {
   try {
     var result = await axios.get(`https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?api_key=${process.env.API_KEY}`);
     var game_id=[];
 
-    for(var i=0;i< process.env.GAME_TIMES;i++){
+    for(var i=0 + add_game_count;i< process.env.GAME_TIMES + add_game_count;i++){
       game_id.push(result.data.matches[i].gameId);
     }
 
--- routes/summonerInfo.js
+++ routes/summonerInfo.js
@@ -4,32 +4,34 @@ var getSummonerInfo = require('../axios/summonerInfo');
 var getSummonerId = require('../axios/summonerId');
 var getGameId = require('../axios/getGameId');
 
-
 var matchDto_JS = require('../routerJS/summonerInfo/matchDto.js');
 
+var searchedName;
+var summoner_getGameId;
+var add_game_count;
+var searchedSummonerId;
 /* GET users listing. */
 router.get('/', function (req, res, next) {
 
     const getSummoner_information = async () => {
         try {
-            const searchedName = req.query.summonerName; //소환사 이름 ex) 여의도 한량이
-            console.log(searchedName);
-            console.time("getSummonerId");
-            const searchedSummonerId = await getSummonerId(searchedName); //쿼리스트링의 이름을 axios.getSummonerId에 넣어서 값 받기 AccountId : searchedSummonerId.data.accountId
-            console.timeEnd("getSummonerId");
-            console.time("getSummonerInfo");
-            var summoner = await getSummonerInfo(searchedSummonerId.data.id); //소환사 기본 정보 콜백함수
-            console.timeEnd("getSummonerInfo");
-            const summoner_getGameId = await getGameId(searchedSummonerId.data.accountId); //gameId : summoner_getGameId.data.matches[0].gameId
-           
-            const participantList = await matchDto_JS(summoner_getGameId, searchedName); 
+            add_game_count=0;
+            searchedName = req.query.summonerName; //소환사 이름 ex) 여의도 한량이
+
+            searchedSummonerId = await getSummonerId(searchedName); //쿼리스트링의 이름을 axios.getSummonerId에 넣어서 값 받기 AccountId : searchedSummonerId.data.accountId
             
+            var summoner = await getSummonerInfo(searchedSummonerId.data.id); //소환사 기본 정보 콜백함수
+
+            summoner_getGameId = await getGameId(searchedSummonerId.data.accountId, add_game_count); //gameId : summoner_getGameId.data.matches[0].gameId
+           
+            const participantList = await matchDto_JS(summoner_getGameId, searchedName);
+           
             res.render('summonerInfo.pug', {
-                summoner_query : searchedName,
+                summoner_query: searchedName,
                 summoner: summoner,
                 result: participantList,
             });
-           
+
         } catch (error) {
             console.log(error);
         }
@@ -38,26 +40,19 @@ router.get('/', function (req, res, next) {
     getSummoner_information();
 });
 
-router.get('/ajax',function(req, res, next){
+router.get('/add_summoner', function (req, res, next) {
     const getSummoner_information = async () => {
-        var summoner={};
+
         try {
-            const searchedName = req.query.summoner_query; //소환사 이름 ex) 여의도 한량이
-            console.log(searchedName);
-            console.time("getSummonerId_ajax");
-            const searchedSummonerId = await getSummonerId(searchedName); //쿼리스트링의 이름을 axios.getSummonerId에 넣어서 값 받기 AccountId : searchedSummonerId.data.accountId
-            console.timeEnd("getSummonerId_ajax");
-            console.time("getSummonerInfo_ajax");
-            summoner = await getSummonerInfo(searchedSummonerId.data.id); //소환사 기본 정보 콜백함수
-            console.timeEnd("getSummonerInfo_ajax");
-            const summoner_getGameId = await getGameId(searchedSummonerId.data.accountId); //gameId : summoner_getGameId.data.matches[0].gameId
+            add_game_count+=3;
+            summoner_getGameId = await getGameId(searchedSummonerId.data.accountId, add_game_count);
+            const participantList = await matchDto_JS(summoner_getGameId, searchedName);
+            console.log(add_game_count);
            
-            const participantList = await matchDto_JS(summoner_getGameId, searchedName); 
-    
-           res.json({
-                participantList : participantList,
-           })
-           
+            res.json({
+                participantList: participantList,
+            })
+
         } catch (error) {
             console.log(error);
         }
