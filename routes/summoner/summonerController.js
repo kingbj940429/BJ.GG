53e815bd33b6cce8059dc54385396ed6c7fefc57
kingbj940429 <kingbj0429@gmail.com>
2020-12-02 22:51:37 +0900

    feat : pug 전편 수정

--- routes/summoner/summonerController.js
+++ routes/summoner/summonerController.js
@@ -3,19 +3,20 @@ var router = express.Router();
 const summonerService = require('./summonerService');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    const summonerName = req.query['summonerName'];
+// router.get('/', function (req, res, next) {
+//     const summonerName = req.query['summonerName'];
 
-    res.render('./summoner/summonerLayout', {
-        summonerName,
-    })
-});
+//     res.render('./summoner/summonerLayout', {
+//         summonerName,
+//     })
+// });
 
 router.get('/summonerList', async (req, res, next)=>{
     var result = await summonerService.summonerList(req, res);
     console.log(result);
-    res.json({
-        test:"성공",
+    console.log(result[2].all_summoners);
+    res.render('./summoner/summonerGameHistory', {
+        result,
     })
 });
 
